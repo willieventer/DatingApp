96fb6f2335a365c7d64e81000eb621257a5f54d8
Willie Venter <williec.venter@gmail.com>
2023-11-20 19:20:34 +0200

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,12 +12,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
 
 }
 );
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 
 app.UseAuthorization();
 
